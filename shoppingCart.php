eb66638ffdc0d094fccb150a8f659ebd3609e592
benmertz <bn.mertz@gmail.com>
2017-04-09 20:05:19 -0600

    Implemented shopping cart
    Cannot currently delete items in the cart.

--- shoppingCart.php
+++ shoppingCart.php
@@ -4,17 +4,67 @@ require_once "inc/page_setup.php";
 
 $pgTitle = "shoppingCart";
 include ('inc/header.php');
+$db = new Database();
 
 ?>
 
 </head>
 
 <?php include ('inc/nav.php');?>
-
+<?php
+function sendEmail($db) {
+	$items = $_SESSION['shoppingCart'];
+	$_SESSION['shoppingCart'] = array();
+	$contents = "";
+	foreach($items as $a)
+		$contents .= $a . ", ";
+	
+	if (strcmp($contents, "") == 0)
+		return;
+	
+	$sql = "SELECT * FROM users WHERE role='1'";
+	$admins = $db->prepare($sql);
+	$admins->execute();
+	
+	$msgBody = "An order has been placed for the following items: " . $contents;
+	foreach ($admins as $a)
+		mail($a['email'], "Shopping Cart", $msgBody);
+		
+	$sql = "SELECT email FROM users WHERE username='" . $_SESSION['sessionUser'] . "'";
+	$email = $db->prepare($sql);
+	$email->execute();
+	$email = $email->fetch();
+	$sendTo = $email['email'];
+	
+	mail($sendTo, "Shopping Cart", $msgBody);
+	
+	header ("Location: ./shoppingCart.php");
+}
+?>
 <!-- Start contents of main page here. -->
 
 <div class="container col-xs-12">
-	<h3>Add Shopping Cart functionality here</h3>
+	<div align="center">
+		<?php
+			if(isset($_GET['i'])){
+				array_push($_SESSION['shoppingCart'], $_GET['i']);
+			}
+			
+			echo "<h3>Shopping Cart:</h3>";
+			$count = 1;
+			foreach($_SESSION['shoppingCart'] as $t) {
+				echo "$count: <strong>$t</strong><br>";
+				$count = $count + 1;
+			}
+		?>
+		<?php
+			if(isset($_POST['submit']))
+				sendEmail($db);
+		?>
+		<form action="./shoppingCart.php" method="POST">
+			<input type="submit" name="submit" value="Checkout">
+		</form>
+	</div>
 </div>
 
 <!-- End of contents -->
